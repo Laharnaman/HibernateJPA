94a62cf9f72b826dcd3b114afcd64281c300d620
Bob Kelly <bobkell@gmail.com>
2017-08-31 08:26:12 +0100

    JPA query api using builder and hibernate meta model code generation. Enables better type saftety. Difficult to use however

--- src/com/bob/testharness/JPAChapter20.java
+++ src/com/bob/testharness/JPAChapter20.java
@@ -13,14 +13,24 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Selection;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
+import com.bob.domain.Address;
+import com.bob.domain.Address_;
 import com.bob.domain.Student;
+import com.bob.domain.Student_;
 import com.bob.domain.Subject;
 import com.bob.domain.Tutor;
+import com.bob.domain.Tutor_;
 
 public class JPAChapter20 {
 	public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA_MYSQL_CONFIG");
@@ -32,37 +42,66 @@ public class JPAChapter20 {
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 
-		// Don't use this method in production. Code will no longer be portable
-		// (JPA restrictions)
-		Session session = (Session) em.getDelegate();
-
-		Criteria studentCriteria = session.createCriteria(Student.class);
-		String myname = "David Banks";
-		list = studentCriteria.createCriteria("supervisor").add(Restrictions.eq("name", myname)).list();
-
-		// Use criteria API: Tutors with more than 1 student.
-		Criteria tutorCriteria = session.createCriteria(Tutor.class);
-	//	tutorCriteria.add(Restrictions.sizeGt("supervisionGroup", 1)).list().stream().forEach(System.out::println);
-
-		// Using HQL: same thing
-//		em.createQuery("select tutor from Tutor as tutor where tutor.supervisionGroup.size > 1").getResultList()
-//				.stream().forEach(System.out::println);
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		//CriteriaQuery<Student> criteria = builder.createQuery(Student.class);
 		
-		//Critia API: list of tutors that have a student living in georgia
-		Criteria tutorGeorgia = session.createCriteria(Tutor.class);
-		tutorGeorgia
-			.add(Restrictions.sizeNe("supervisionGroup", 0))
-			.createAlias("supervisionGroup", "student")
-			.add(Restrictions.like("student.address.city", "%Georg%"))
-			.list()
-			.stream()
-			.forEach(System.out::println);
+		/**
+		 * Using the meta model generator from Hibernate. 
+		 * Benefits: better type safety; 
+		 * downside: extra compilation step via hibernate metamodel code generation.
+		 * Scans all entities and makes attributes public via a 'cloned' class
+		 * How to activate:
+		 * prefs > Java Compiler > Annotation processing: change to ./generated for the path
+		 * prefs > Java Compilater > Annotation processing > Factory path: add the two hibernate jars
+		 *    jpamodelgen and jpa-2.0-api
+		 */
 		
-		//HQL
-		/*	em.createQuery("select tutor from Tutor tutor join tutor.supervisionGroup as student where student.address.city like '%G%'")
-		.getResultList()
-		.stream()
-		.forEach(System.out::println);*/
+		CriteriaQuery<Tutor> criteria = builder.createQuery(Tutor.class).distinct(true);
+		Root<Tutor> root = criteria.from(Tutor.class);
+		Join<Tutor, Student> students = root.join(Tutor_.supervisionGroup);
+		
+		Path<Address> address = students.get(Student_.address);
+		Path<String> city = address.get(Address_.city);
+		criteria.where(builder.equal(city, "Georgia"));
+		
+		TypedQuery<Tutor> q = em.createQuery(criteria);
+		q.getResultList().stream().forEach(System.out::println);
+		
+		
+		/**
+		 * tutors whose student lives in Georgia
+		 */
+//		CriteriaQuery<Tutor> criteria = builder.createQuery(Tutor.class).distinct(true);
+//		Root<Tutor> root = criteria.from(Tutor.class);
+//		Join<Tutor, Student> students = root.join("supervisionGroup");
+//		
+//		Path<Address> address = students.get("address");
+//		Path<String> city = address.get("city");
+//		criteria.where(builder.equal(city, "Georgia"));
+		
+		
+		/**
+		 * get students who have letter e in their name
+		 */
+//		Root<Student> root = criteria.from(Student.class);
+//		criteria.where(builder.like(root.get("name"), "%e%"));
+		
+		/**
+		 * Select students where supervisor is david banks
+		 */
+//		Path<Tutor> tutor = root.get("supervisor");
+//		criteria.where(builder.equal(tutor.get("name"), "David Banks"));
+		
+		//17:00
+		//criteria.where(builder.equal(root.get("name"), "Marco Fortes"));
+		
+		
+		//Query q = em.createQuery(criteria);
+		
+		//q.getResultList().stream().forEach(System.out::println);
+		
+		
+		
 	
 		tx.commit();
 		em.close();
@@ -103,7 +142,7 @@ public class JPAChapter20 {
 		// this only works because we are cascading from tutor to student
 		t1.createStudentAndAddToSupervisionGroup("Marco Fortes", "1-FOR-2010", "1 The Street", "Georgia", "484848");
 		t1.createStudentAndAddToSupervisionGroup("Kath Grainer", "2-GRA-2009", "2 Kaths Street", "Geor", "939393");
-		t3.createStudentAndAddToSupervisionGroup("Sandra Perkins", "3-PER-2009", "4 The Avenue", "Georgia", "939393");
+		t3.createStudentAndAddToSupervisionGroup("Sandra Perkins", "3-PER-2009", "4 The Avenue", "New York", "939393");
 
 		tx.commit();
 		em.close();
